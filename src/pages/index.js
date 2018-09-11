b61e09167bdbecadd3d767e7c9bb66780f174758
Lokanath c <lokanathc@LokanathMacBookPro.local>
2018-09-10 23:52:47 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi fellow coders</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>My name is <strong>Lokanath ChandraReddy.</strong> I am a recent Masters in Computer Science Graduate at <i>University of Massachusetts Lowell</i>. </p>
+    <p> I am a <strong>freecodeprogrammer </strong>with a passion to work and explore web development.</p>
+    <p> My Skill stack includes the following 
+      <ul>
+        <li> HTML5</li>
+        <li>Angular 4</li>
+        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>Java</li>
+        <li>MongoDB</li>
+        <li>CSS</li>
+        <li>PeopleCode</li>
+        <li>D3.js</li>
+      </ul>
+    </p>
+    <p>The courses Internet and Web Systems (I and II), Computer Graphics, Internet of Things among others, provides a solid background for my interest in Web Development. As a leader of a team of four members, we designed a web portal for news students to go through the reviews posted by the seniors about which courses to choose from and under which professor. We used the MEAN stack (with Angular2+), implemented our own RESTful API’s and have relied on the knowledge of Angular 2, HTML5, Bootstrap, NodeJS, MongoDB, AWS, CSS3 for developing the website. I have worked on projects based on IOT (python, HTML, JavaScript), Graphics (2D and 3D) (Three.js) and implemented my skills to a full extent on the said projects</p>
+    <p>Being a passionate photographer and interest with graphics, always looked forward to being creative and unique, which I have implemented in the projects that I have worked on both in my earlier company and course-based projects. Have always followed the motto - Create, Develop, Innovate and repeat the cycle. Attending meetups (Angular Boston, Boston CHI, Boston JavaScript), conferences such as Fluent, Mongo World, Node Summit (volunteer) has helped to be updated about the latest technologies</p>
+
+    <p>More than everything above, I have the passion, love to help the community, volunteer at events and to participate in hackathons</p>
+
+    <p>For more information or questions please send an email to <strong>lokanathchandrareddy@gmail.com</strong> </p>
+    <ul> <Link to="https://github.com/lokanathchandrareddy/">Check out my Github Profile </Link> </ul>
+    <ul><Link to="https://www.linkedin.com/in/lokanathchandrareddy">Check out my linkedin Profile </Link></ul>
+  </div>
+)
+
+export default IndexPage
